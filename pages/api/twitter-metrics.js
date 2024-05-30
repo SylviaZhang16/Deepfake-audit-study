b591fc0e4a3388d31c4599e69db698720101dd29
zshihui16@gmail.com <sylviaboom16@gmail.com>
2024-05-30 12:38:54 -0400

    convert twitter script to js

--- pages/api/twitter-metrics.js
+++ pages/api/twitter-metrics.js
@@ -9,7 +9,7 @@ const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 const getTwitterPostMetrics = async (email, username, password, postUrls) => {
   const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36";
 
-  const browser = await puppeteer.launch({ headless: false,
+  const browser = await puppeteer.launch({ headless: true,
     defaultViewport: {
       width: 1920,
       height: 1080,
