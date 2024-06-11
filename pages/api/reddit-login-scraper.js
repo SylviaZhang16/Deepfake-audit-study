46a76bc606ae287334cdcc5da77bb3e497159c2e
zshihui16@gmail.com <sylviaboom16@gmail.com>
2024-06-11 19:17:05 -0400

    webkit config

--- pages/api/reddit-login-scraper.js
+++ pages/api/reddit-login-scraper.js
@@ -34,13 +34,13 @@ const scrapeDataForUser = async (username, password, callback) => {
     const browser = await webkit.launch({
       headless: true,
       args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
-        '--disable-accelerated-2d-canvas',
-        '--disable-gpu',
-        '--no-zygote',
-        '--disable-software-rasterizer'
+        // '--no-sandbox',
+        // '--disable-setuid-sandbox',
+        // '--disable-dev-shm-usage',
+        // '--disable-accelerated-2d-canvas',
+        // '--disable-gpu',
+        // '--no-zygote',
+        // '--disable-software-rasterizer'
       ]
     });
 
